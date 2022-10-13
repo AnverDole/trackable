c52007ead57a22d4e66df428cb70ab91852a7aed
Anver Dole <mohomadanver@gmail.com>
2022-10-14 03:49:36 +0530

    Bug fixed in profile management

--- resources/views/user-system-admin/profile-management/index.blade.php
+++ resources/views/user-system-admin/profile-management/index.blade.php
@@ -19,9 +19,6 @@
 
 @section('content-main')
     <main class="menu-content-body">
-        @yield('admin.management.navigation')
-
-
         <div id="update-profile" class="bg-white shadow-lg w-100 rounded p-4">
             <h4>Edt Profile #{{ $user->id }}</h4>
             <p>All fields are necessary.</p>
@@ -38,17 +35,17 @@
                 </div>
             @enderror
 
-            <form action="{{ route('profile-management.edit') }}" method="post">
+            <form action="{{ route('parent.profile-management.edit') }}" method="post">
                 @csrf
 
-                <div  class="mt-4">
+                <div class="mt-4">
                     <div class="form-floating">
                         <input type="text"
-                            class="form-control   @error('updateProfile', 'firstname') is-invalid @enderror" id="firstname"
-                            name="firstname" placeholder="First Name"
-                            value="{{ old('updateProfile', 'firstname') ?? $user->firstname }}" required>
+                            class="form-control   @error('firstname', 'updateProfile') is-invalid @enderror" id="firstname"
+                            name="firstname" placeholder="First Name" value="{{ old('firstname') ?? $user->firstname }}"
+                            required>
                         <label for="firstname">First Name</label>
-                        @error('updateProfile', 'firstname')
+                        @error('firstname', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -56,33 +53,33 @@
                     </div>
                     <div class="form-floating mt-2">
                         <input type="text"
-                            class="form-control   @error('updateProfile', 'lastname') is-invalid @enderror" id="lastname"
-                            name="lastname" placeholder="Last name"
-                            value="{{ old('updateProfile', 'lastname') ?? $user->lastname }}" required>
+                            class="form-control   @error('lastname', 'updateProfile') is-invalid @enderror" id="lastname"
+                            name="lastname" placeholder="Last name" value="{{ old('lastname') ?? $user->lastname }}"
+                            required>
                         <label for="lastname">Last Name</label>
-                        @error('updateProfile', 'lastname')
+                        @error('lastname', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
                         @enderror
                     </div>
                     <div class="form-floating mt-2">
-                        <input type="email" class="form-control @error('updateProfile', 'email') is-invalid @enderror"
+                        <input type="email" class="form-control @error('email', 'updateProfile') is-invalid @enderror"
                             id="email" name="email" placeholder="Email address"
                             value="{{ old('email') ?? $user->email }}" required>
                         <label for="email">Email</label>
-                        @error('updateProfile', 'email')
+                        @error('email', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
                         @enderror
                     </div>
                     <div class="form-floating mt-2">
-                        <input type="tel" class="form-control @error('updateProfile', 'telephone') is-invalid @enderror"
+                        <input type="tel" class="form-control @error('telephone', 'updateProfile') is-invalid @enderror"
                             id="telephone" name="telephone" placeholder="Telephone"
                             value="{{ old('telephone') ?? $user->telephone }}" required>
                         <label for="telephone">Telephone</label>
-                        @error('updateProfile', 'telephone')
+                        @error('telephone', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -92,11 +89,11 @@
                     <label for="address_line_1" class="mt-4 mb-2">Residence</label>
                     <div class="form-floating">
                         <input type="text"
-                            class="form-control @error('updateProfile', 'address_line_1') is-invalid @enderror"
+                            class="form-control @error('address_line_1', 'updateProfile') is-invalid @enderror"
                             id="address_line_1" name="address_line_1" placeholder="Address line 1"
                             value="{{ old('address_line_1') ?? $user->address_line_1 }}" required>
                         <label for="address_line_1">Address Line 1</label>
-                        @error('updateProfile', 'address_line_1')
+                        @error('address_line_1', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -104,11 +101,11 @@
                     </div>
                     <div class="form-floating mt-2">
                         <input type="text"
-                            class="form-control @error('updateProfile', 'address_line_2') is-invalid @enderror"
+                            class="form-control @error('address_line_2', 'updateProfile') is-invalid @enderror"
                             id="address_line_2" name="address_line_2" placeholder="Address line 2"
                             value="{{ old('address_line_2') ?? $user->address_line_2 }}" required>
                         <label for="address_line_2">Address Line 2</label>
-                        @error('updateProfile', 'address_line_2')
+                        @error('address_line_2', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -116,11 +113,11 @@
                     </div>
                     <div class="form-floating mt-2">
                         <input type="text"
-                            class="form-control @error('updateProfile', 'address_line_3') is-invalid @enderror"
+                            class="form-control @error('address_line_3', 'updateProfile') is-invalid @enderror"
                             id="address_line_3" name="address_line_3" placeholder="Address line 3"
                             value="{{ old('address_line_3') ?? $user->address_line_3 }}">
                         <label for="address_line_3">Address Line 3</label>
-                        @error('updateProfile', 'address_line_3')
+                        @error('address_line_3', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -128,7 +125,7 @@
                     </div>
 
                     <div class="form-floating mt-2">
-                        <select class="form-select @error('updateProfile', 'city') is-invalid @enderror" id="city"
+                        <select class="form-select @error('city', 'updateProfile') is-invalid @enderror" id="city"
                             name="city" aria-label="City" required>
                             <option selected disabled value="-1">Choose one</option>
                             @foreach ($cities as $city)
@@ -137,7 +134,7 @@
                             @endforeach
                         </select>
                         <label for="city">City</label>
-                        @error('updateProfile', 'city')
+                        @error('city', 'updateProfile')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -166,24 +163,15 @@
                 </div>
             @enderror
 
-            <form action="{{ route('profile-management.update-password') }}" method="post">
+            <form action="{{ route('parent.profile-management.update-password') }}" method="post">
                 @csrf
                 <div class="mt-4">
                     <div class="form-floating">
-                        <input type="password" class="form-control   @error('current_password') is-invalid @enderror"
+                        <input type="password"
+                            class="form-control   @error('current_password', 'passwordChange') is-invalid @enderror"
                             id="current_password" name="current_password" placeholder="Type school's name" required>
                         <label for="current_password">Current Password</label>
-                        @error('password-change', 'current_password')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="form-floating mt-2">
-                        <input type="password" class="form-control   @error('new_password') is-invalid @enderror"
-                            id="new_password" name="new_password" placeholder="Type school's name" required>
-                        <label for="new_password">New Password</label>
-                        @error('password-change', 'new_password')
+                        @error('password-change', 'passwordChange')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -191,11 +179,22 @@
                     </div>
                     <div class="form-floating mt-2">
                         <input type="password"
-                            class="form-control   @error('new_password_confirmation') is-invalid @enderror"
+                            class="form-control   @error('new_password', 'passwordChange') is-invalid @enderror"
+                            id="new_password" name="new_password" placeholder="Type school's name" required>
+                        <label for="new_password">New Password</label>
+                        @error('password-change', 'passwordChange')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating mt-2">
+                        <input type="password"
+                            class="form-control   @error('new_password_confirmation', 'passwordChange') is-invalid @enderror"
                             id="new_password_confirmation" name="new_password_confirmation"
                             placeholder="Type school's name" required>
                         <label for="new_password_confirmation">Retype New Password</label>
-                        @error('password-change', 'new_password_confirmation')
+                        @error('password-change', 'passwordChange')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
